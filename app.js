9e98bacd9d8d24529e282c4392a8489192e3e98d
Dmitriy Dovgan <d_dovgan@wargaming.net>
2019-05-05 16:17:11 +0300

    master. Add eslint

--- app.js
+++ app.js
@@ -3,11 +3,9 @@
 const Homey = require('homey');
 
 class MyApp extends Homey.App {
-	
-	onInit() {
-		this.log('MyApp is running...');
-	}
-	
+    onInit() {
+        this.log('MyApp is running...');
+    }
 }
 
 module.exports = MyApp;
