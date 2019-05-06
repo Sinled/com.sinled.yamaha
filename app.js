a531bf59d0537310a9c5ce0bcf115ec7c7f3e704
Dmitriy Dovgan <d_dovgan@wargaming.net>
2019-05-06 19:05:04 +0300

    master. Add device icon

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
 'use strict';
 
 const Homey = require('homey');
-const Yamaha = require('yamaha-yxc-nodejs');
+// const Yamaha = require('yamaha-yxc-nodejs');
 
 class MyApp extends Homey.App {
     async onInit() {
         this.log('MyApp is running...');
-        const yamaha = new Yamaha('192.168.1.102');
-        const startTime = process.hrtime();
-        const result = await yamaha.getStatus();
-        const data = JSON.parse(result);
-        console.log(data);
-        const endTime = process.hrtime(startTime);
-        console.log(endTime);
+        // const yamaha = new Yamaha('192.168.1.102');
+        // const startTime = process.hrtime();
+        // const result = await yamaha.getStatus();
+        // const data = JSON.parse(result);
+        // console.log(data);
+        // const endTime = process.hrtime(startTime);
+        // console.log(endTime);
         // console.log(result);
         // console.log('dest  ' + att.destination);
         // console.log('api  ' + att.api_version);
