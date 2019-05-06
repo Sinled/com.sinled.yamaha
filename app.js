817bb6b9daf88a23d4fdd55b2a22eb280835f4ad
Dmitriy Dovgan <d_dovgan@wargaming.net>
2019-05-06 18:07:59 +0300

    master. Add yamaha node

--- app.js
+++ app.js
@@ -1,10 +1,22 @@
 'use strict';
 
 const Homey = require('homey');
+const Yamaha = require('yamaha-yxc-nodejs');
 
 class MyApp extends Homey.App {
-    onInit() {
+    async onInit() {
         this.log('MyApp is running...');
+        const yamaha = new Yamaha('192.168.1.102');
+        const startTime = process.hrtime();
+        const result = await yamaha.getStatus();
+        const data = JSON.parse(result);
+        console.log(data);
+        const endTime = process.hrtime(startTime);
+        console.log(endTime);
+        // console.log(result);
+        // console.log('dest  ' + att.destination);
+        // console.log('api  ' + att.api_version);
+        // console.log(att);
     }
 }
 
